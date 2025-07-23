4c6754aeefedd2a90bfd29e82be88343b86727d7
william <wallacewilliam076@gmail.com>
2025-07-23 17:54:00 -0300

    validacaoParcialInputs

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,12 +1,16 @@
 import './Forms.css'
 
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form"
 
 const Forms = () => {
 
-    const {register, handleSubmit} = useForm()
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
 
-    const onSubmit = (data,event) => {console.log(data); event.preventDefault(); };
+    const onSubmit = (data, event) => { console.log(data); event.preventDefault(); };
 
     return (
         <>
@@ -22,97 +26,136 @@ const Forms = () => {
             <section className='main-section'>
 
                 <div className="form-area">
-                        <form id="form-predicao">
-                            <label>Idade:</label>
-                            <input 
-                            type="number" 
-                            name="idade" 
-                            min="16" max="90"
-                            required 
-                            {...register("idade")}
-                            
-                            /><br />
+                    <form id="form-predicao">
+                        <label>Idade:</label>
+                        <input
+                            className={errors?.idade && "input-error"}
+                            type="number"
+                            name="idade"
+                            min = "16"
+                            max="90"
+                            placeholder="Digite sua idade"
+                            {...register("idade", { required: true, min:16, max:90 })}
 
-                            <label>Gênero:</label>
-                            <select name="genero" {...register("genero")}>
-                                <option value="masculino">Masculino</option>
-                                <option value="feminino">Feminino</option>
-                                <option value="outro">Outro</option>
-                            </select><br />
+                        />
+                        {errors?.idade?.type === "required" && (<p className='error-message'>Campo obrigatório</p>) ||
+                        errors?.idade?.type==="min" && (<p className='error-message'>A idade minima é 16 anos</p>)  ||
+                        errors?.idade?.type==="max" && (<p className='error-message'>A idade máxima é 90 anos</p>)
+                        }
+                        <br />
 
-                            <label>Etnia:</label>
-                            <input 
-                            type="text" 
+                        <label>Gênero:</label>
+                        <select name="genero" {...register("genero", { required: true })}>
+                            <option value="">Escolha</option>
+                            <option value="masculino">Masculino</option>
+                            <option value="feminino">Feminino</option>
+                            <option value="outro">Outro</option>
+                        </select>
+                        {errors?.genero?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
+
+                        <label>Etnia:</label>
+                        <input
+                            className={errors?.etnia && "input-error"}
+                            type="text"
                             name="etnia"
-                            {...register("etnia")}
-                             /><br />
+                            {...register("etnia", { required: true})}
+                        />
+                        {errors?.etnia?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <label>PCD (Pessoa com Deficiência):</label>
-                            <select name="pcd" {...register("pcd")}>
-                                <option value="sim">Sim</option>
-                                <option value="nao">Não</option>
-                            </select><br />
+                        <label>PCD (Pessoa com Deficiência):</label>
+                        <select name="pcd" {...register("pcd", { required: true })}>
+                            <option value="">Escolha</option>
+                            <option value="sim">Sim</option>
+                            <option value="nao">Não</option>
+                        </select>
+                        {errors?.pcd?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <label>Vive no Brasil:</label>
-                            <select name="vive_no_brasil" {...register("viveBrasil")}>
-                                <option value="sim">Sim</option>
-                                <option value="nao">Não</option>
-                            </select><br />
+                        <label>Vive no Brasil:</label>
+                        <select name="vive_no_brasil" {...register("viveBrasil", { required: true })}>
+                            <option value="">Escolha</option>
+                            <option value="sim">Sim</option>
+                            <option value="nao">Não</option>
+                        </select>
+                        {errors?.viveBrasil?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <label>Estado de Moradia:</label>
-                            <input 
-                            type="text" 
-                            name="estado_moradia" 
-                            {...register("estadoMoradia")}
-                            /><br />
+                        <label>Estado de Moradia:</label>
+                        <input
+                            className={errors?.estadoMoradia && "input-error"}
+                            type="text"
+                            name="estado_moradia"
+                            {...register("estadoMoradia", { required: true })}
+                        />
+                        {errors?.estadoMoradia?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <label>Nível de Ensino:</label>
-                            <select name="nivel_ensino" {...register("nivelEnsino")}>
-                                <option value="fundamental">Fundamental</option>
-                                <option value="medio">Médio</option>
-                                <option value="superior">Superior</option>
-                                <option value="pos">Pós-graduação</option>
-                            </select><br />
+                        <label>Nível de Ensino:</label>
+                        <select name="nivel_ensino" {...register("nivelEnsino", { required: true })}>
+                            <option value="">Escolha</option>
+                            <option value="fundamental">Fundamental</option>
+                            <option value="medio">Médio</option>
+                            <option value="superior">Superior</option>
+                            <option value="pos">Pós-graduação</option>
+                        </select>
+                        {errors?.nivelEnsino?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <label>Formação:</label>
-                            <input 
-                            type="text" 
+                        <label>Formação:</label>
+                        <input
+                            className={errors?.formacao && "input-error"}
+                            type="text"
                             name="formacao"
-                            {...register("formacao")}
-                            /><br />
+                            {...register("formacao", { required: true })}
+                        />
+                        {errors?.formacao?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}<br />
 
-                            <label>Tempo de Experiência com Dados (anos):</label>
-                            <input 
-                            type="number" 
+                        <label>Tempo de Experiência com Dados (anos):</label>
+                        <input
+                            className={errors?.experienciaDados && "input-error"}
+                            type="number"
                             name="tempo_experiencia_dados"
-                            {...register("experienciaDados")}
-                            /><br />
+                            {...register("experienciaDados", { required: true })}
+                        />
+                        {errors?.experienciaDados?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <label>Linguagens Preferidas (ex: Python, R):</label>
-                            <input 
-                            type="text" 
+                        <label>Linguagens Preferidas (ex: Python, R):</label>
+                        <input
+                            className={errors?.linguagensPreferidas && "input-error"}
+                            type="text"
                             name="linguagens_preferidas"
-                            {...register("linguagensPreferidas")}
-                             /><br />
+                            {...register("linguagensPreferidas", { required: true })}
+                        />
+                        {errors?.linguagensPreferidas?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <label>Bancos de Dados (ex: MySQL, MongoDB):</label>
-                            <input 
-                            type="text" 
+                        <label>Bancos de Dados (ex: MySQL, MongoDB):</label>
+                        <input
+                            className={errors?.bancoDados && "input-error"}
+                            type="text"
                             name="bancos_de_dados"
-                            {...register("bancoDados")}
-                             /><br />
+                            {...register("bancoDados", { required: true })}
+                        />
+                        {errors?.bancoDados?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <label>Cloud Preferida:</label>
-                            <select name="cloud_preferida" {...register("cloudPreferida")}>
-                                <option value="aws">AWS</option>
-                                <option value="azure">Azure</option>
-                                <option value="gcp">GCP</option>
-                                <option value="nenhuma">Nenhuma</option>
-                            </select><br />
+                        <label>Cloud Preferida:</label>
+                        <select name="cloud_preferida" {...register("cloudPreferida", { required: true })}>
+                            <option value="">Escolha</option>
+                            <option value="aws">AWS</option>
+                            <option value="azure">Azure</option>
+                            <option value="gcp">GCP</option>
+                            <option value="nenhuma">Nenhuma</option>
+                        </select>
+                        {errors?.cloudPreferida?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                        <br />
 
-                            <button type="" onClick={()=> handleSubmit(onSubmit)()}>Enviar</button>
-                        </form>
-                    </div>
+                        <button type="" onClick={() => handleSubmit(onSubmit)()}>Enviar</button>
+                    </form>
+                </div>
 
             </section>
 
