66b19a9073e5f41aaa6266394f42e7907c8c0317
william <wallacewilliam076@gmail.com>
2025-07-27 17:44:51 -0300

    endpoint adicionado

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -30,7 +30,7 @@ const Forms = () => {
                 cloud_preferida: data.cloudPreferida
             };
 
-            const response = await axios.post('https://NOSSO_WEBHOOK_N8N.com', payload);
+            const response = await axios.post('http://localhost:5678/webhook/previsao', payload);
             setCargoPrevisto(response.data.cargo) // cargo retornado pelo agente 
         } catch (error) {
             console.error('Erro ao enviar os dados', error);
