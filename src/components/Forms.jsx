317cb5cbce2fb0579b1f0600c5f8d1d05a39a69f
Raiane <hihae1311@gmail.com>
2025-08-25 14:20:37 -0300

    fix: atualizar opções de seleção e corrigir formatação

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -83,28 +83,32 @@ const Forms = () => {
                             <label>Gênero:</label>
                             <select name="genero" {...register("genero", { required: true })}>
                                 <option value="">Escolha</option>
-                                <option value="masculino">Masculino</option>
-                                <option value="feminino">Feminino</option>
-                                <option value="outro">Outro</option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Feminino">Feminino</option>
+                                {/* <option value="Prefiro não informar">Prefiro não informar</option> */}
                             </select>
                             {errors?.genero?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
                             <br />
 
                             <label>Etnia:</label>
-                            <input
-                                className={errors?.etnia && "input-error"}
-                                type="text"
-                                name="etnia"
-                                {...register("etnia", { required: true })}
-                            />
+                            <select name="etnia" {...register("etnia", { required: true })}>
+                                <option value="">Escolha</option>
+                                <option value="Branca">Branca</option>
+                                <option value="Parda">Parda</option>
+                                <option value="Preta">Preta</option>
+                                <option value="Amarela">Amarela</option>
+                                {/* <option value="Prefiro não informar">Prefiro não informar</option> */}
+                                {/* <option value="Indígena">Indígena</option> */}
+                                {/* <option value="Outra">Outra</option> */}
+                            </select>
                             {errors?.etnia?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
                             <br />
 
                             <label>PCD (Pessoa com Deficiência):</label>
                             <select name="pcd" {...register("pcd", { required: true })}>
                                 <option value="">Escolha</option>
-                                <option value="sim">Sim</option>
-                                <option value="nao">Não</option>
+                                <option value="Sim">Sim</option>
+                                <option value="Não">Não</option>
                             </select>
                             {errors?.pcd?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
                             <br />
@@ -112,64 +116,103 @@ const Forms = () => {
                             <label>Vive no Brasil:</label>
                             <select name="vive_no_brasil" {...register("viveBrasil", { required: true })}>
                                 <option value="">Escolha</option>
-                                <option value="sim">Sim</option>
-                                <option value="nao">Não</option>
+                                <option value="Sim">Sim</option>
+                                <option value="Não">Não</option>
                             </select>
                             {errors?.viveBrasil?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
                             <br />
 
                             <label>Estado de Moradia:</label>
-                            <input
-                                className={errors?.estadoMoradia && "input-error"}
-                                type="text"
-                                name="estado_moradia"
-                                {...register("estadoMoradia", { required: true })}
-                            />
+                            <select name="estado_moradia" {...register("estadoMoradia", { required: true })}>
+                                <option value="">Escolha</option>
+                                <option value="Acre (AC)">Acre (AC)</option>
+                                <option value="Alagoas (AL)">Alagoas (AL)</option>
+                                <option value="Amapá (AP)">Amapá (AP)</option>
+                                <option value="Amazonas (AM)">Amazonas (AM)</option>
+                                <option value="Bahia (BA)">Bahia (BA)</option>
+                                <option value="Ceará (CE)">Ceará (CE)</option>
+                                <option value="Distrito Federal (DF)">Distrito Federal (DF)</option>
+                                <option value="Espírito Santo (ES)">Espírito Santo (ES)</option>
+                                <option value="Goiás (GO)">Goiás (GO)</option>
+                                <option value="Maranhão (MA)">Maranhão (MA)</option>
+                                <option value="Mato Grosso (MT)">Mato Grosso (MT)</option>
+                                <option value="Mato Grosso do Sul (MS)">Mato Grosso do Sul (MS)</option>
+                                <option value="Minas Gerais (MG)">Minas Gerais (MG)</option>
+                                <option value="Pará (PA)">Pará (PA)</option>
+                                <option value="Paraíba (PB)">Paraíba (PB)</option>
+                                <option value="Paraná (PR)">Paraná (PR)</option>
+                                <option value="Pernambuco (PE)">Pernambuco (PE)</option>
+                                <option value="Piauí (PI)">Piauí (PI)</option>
+                                <option value="Rio de Janeiro (RJ)">Rio de Janeiro (RJ)</option>
+                                <option value="Rio Grande do Norte (RN)">Rio Grande do Norte (RN)</option>
+                                <option value="Rio Grande do Sul (RS)">Rio Grande do Sul (RS)</option>
+                                <option value="Rondônia (RO)">Rondônia (RO)</option>
+                                <option value="Roraima (RR)">Roraima (RR)</option>
+                                <option value="Santa Catarina (SC)">Santa Catarina (SC)</option>
+                                <option value="São Paulo (SP)">São Paulo (SP)</option>
+                                <option value="Sergipe (SE)">Sergipe (SE)</option>
+                                <option value="Tocantins (TO)">Tocantins (TO)</option>
+                            </select>
                             {errors?.estadoMoradia?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
                             <br />
 
                             <label>Nível de Ensino:</label>
-                            <select name="nivel_ensino" {...register("nivelEnsino", { required: true })}>
+                            <select name="nivel_ensino" {...register("nivelEnsino", { required: true })}>   
                                 <option value="">Escolha</option>
-                                <option value="fundamental">Fundamental</option>
-                                <option value="medio">Médio</option>
-                                <option value="superior">Superior</option>
-                                <option value="pos">Pós-graduação</option>
+                                <option value="Estudante de graduação">Estudante de graduação</option>
+                                <option value="Graduação/Bacharelado">Graduação/Bacharelado</option>
+                                <option value="Pós-graduação">Pós-graduação</option>
+                                <option value="Mestrado">Mestrado</option>
+                                <option value="Doutorado ou Phd">Doutorado ou PhD</option>
+                                <option value="Não tenho graduação formal">Não tenho graduação formal</option>
+                                <option value="Prefiro não informar">Prefiro não informar</option>
                             </select>
                             {errors?.nivelEnsino?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
                             <br />
 
                             <label>Formação:</label>
-                            <input
-                                className={errors?.formacao && "input-error"}
-                                type="text"
-                                name="formacao"
-                                {...register("formacao", { required: true })}
-                            />
+                            <select  name="formacao" {...register("formacao", { required: true })}>
+                                <option value="">Escolha</option>
+                                <option value="Computação / Engenharia de Software / Sistemas de Informação/ TI">Computação / Engenharia de Software / Sistemas de Informação/ TI</option>
+                                <option value="Outras Engenharias">Outras Engenharias</option>
+                                <option value="Economia/ Administração / Contabilidade / Finanças/ Negócios">Economia/ Administração / Contabilidade / Finanças/ Negócios</option>
+                                <option value="Estatística/ Matemática / Matemática Computacional/ Ciências Atuariais">Estatística/ Matemática / Matemática Computacional/ Ciências Atuariais</option>
+                                <option value="Outra opção">Outra opção</option>
+                                <option value="Marketing / Publicidade / Comunicação / Jornalismo">Marketing / Publicidade / Comunicação / Jornalismo</option>
+                                <option value="Química / Física">Química / Física</option>
+                                <option value="Ciências Biológicas/ Farmácia/ Medicina/ Área da Saúde">Ciências Biológicas/ Farmácia/ Medicina/ Área da Saúde</option>
+                                <option value="Ciências Sociais">Ciências Sociais</option>
+                            </select>
                             {errors?.formacao?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}<br />
 
                             <label>Tempo de Experiência com Dados (anos):</label>
-                            <input
-                                className={errors?.experienciaDados && "input-error"}
-                                type="number"
-                                min={0}
-                                max={100}
-                                name="tempo_experiencia_dados"
-                                {...register("experienciaDados", { required: true, min: 0, max: 100 })}
-                            />
-                            {errors?.experienciaDados?.type === "required" && (<p className='error-message'>Campo obrigatório</p>) ||
-                                errors?.experienciaDados?.type === "min" && (<p className='error-message'>o tempo de experiencia deve ser no mínimo 0</p>) ||
-                                errors?.experienciaDados?.type === "max" && (<p className='error-message'>Tempo de experiência fora da faixa esperada</p>)
-                            }
+                            <select name="tempo_experiencia_dados" {...register("experienciaDados", { required: true })}>
+                                <option value="">Escolha</option>
+                                <option value="Menos de 1 ano">Menos de 1 ano</option>
+                                <option value="de 1 a 2 anos">de 1 a 2 anos</option>
+                                <option value="de 3 a 4 anos">de 3 a 4 anos</option>
+                                <option value="de 4 a 6 anos">de 4 a 6 anos</option>
+                                <option value="de 7 a 10 anos">de 7 a 10 anos</option>
+                                <option value="Mais de 10 anos">Mais de 10 anos</option>
+                                <option value="Não tenho experiência na área de dados">Não tenho experiência na área de dados</option>
+                            </select>
+                            {errors?.experienciaDados?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
+                                {/* // errors?.experienciaDados?.type === "min" && (<p className='error-message'>o tempo de experiencia deve ser no mínimo 0</p>) ||
+                                // errors?.experienciaDados?.type === "max" && (<p className='error-message'>Tempo de experiência fora da faixa esperada</p>) */}
+                            
                             <br />
 
                             <label>Linguagens Preferidas (ex: Python, R):</label>
-                            <input
-                                className={errors?.linguagensPreferidas && "input-error"}
-                                type="text"
-                                name="linguagens_preferidas"
-                                {...register("linguagensPreferidas", { required: true })}
-                            />
+                            <select name="linguagens_preferidas" {...register("linguagensPreferidas", { required: true })}>
+                                <option value="">Escolha</option>
+                                <option value="Python">Python</option>
+                                <option value="R">R</option>
+                                <option value="Scala">Scala</option>
+                                <option value="SQL">SQL</option>
+                                <option value="C/C++/C#">C/C++/C#</option>
+                                <option value="Julia">Julia</option>
+                                <option value="Javascript">Javascript</option>
+                            </select>
                             {errors?.linguagensPreferidas?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
                             <br />
 
@@ -186,10 +229,11 @@ const Forms = () => {
                             <label>Cloud Preferida:</label>
                             <select name="cloud_preferida" {...register("cloudPreferida", { required: true })}>
                                 <option value="">Escolha</option>
-                                <option value="aws">AWS</option>
-                                <option value="azure">Azure</option>
-                                <option value="gcp">GCP</option>
-                                <option value="nenhuma">Nenhuma</option>
+                                <option value="Amazon Web Services (AWS)">Amazon Web Services (AWS)</option>
+                                <option value="Azure (Microsoft)">Azure (Microsoft)</option>
+                                <option value="Google Cloud (GCP)">Google Cloud (GCP)</option>
+                                <option value="Outra Cloud">Outra Cloud</option>
+                                <option value="Não sei opinar">Não sei opinar</option>
                             </select>
                             {errors?.cloudPreferida?.type === "required" && (<p className='error-message'>Campo obrigatório</p>)}
                             <br />
